f5818d57204d3ea946c94dcde3c81e772efc748c
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2025-10-21 21:27:04 +0500

    Delete scripts directory

--- scripts/clean-db.js
+++
@@ -1,7 +0,0 @@
-const Verifier = require('../lib/auth-verify');
-
-// Initialize Verifier (minimal, just for cleaning)
-const verifier = new Verifier();
-
-verifier.cleanExpired();
-console.log('Expired OTPs cleaned from database.');
